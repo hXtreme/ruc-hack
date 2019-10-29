48632db08e0865db10da7df1024d8fa618fa18ce
Harsh Parekh <h.x.dev@outlook.com>
2019-10-29 14:52:20 -0400

    Add option to clear all markers

--- html/js/search.js
+++ html/js/search.js
@@ -1,7 +1,14 @@
 function search(position) {
 	var delta = 0.1;
-	var lat = position.coords.latitude;
-	var lng = position.coords.longitude;
+	if (user_marker == null) {
+		var lat = position.coords.latitude;
+		var lng = position.coords.longitude;
+	}
+	else {
+		// TODO:Optionally allow user to find places around a marker.
+		var lat = user_marker.position.lat;
+		var lng = user_marker.position.lng;
+	}
 	for(var i=0; i<fountain.length; i++) {
 		var f = fountain[i];
 		var dlat = Math.abs(f.lat - lat);
